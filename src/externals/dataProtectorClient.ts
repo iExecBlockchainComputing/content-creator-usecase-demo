59fdcdd8d4205facf198d02d3ba911e142b413c9
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2024-09-26 17:37:49 +0200

    Move from dataprotector-sdk repo

---
+++ src/externals/dataProtectorClient.ts
@@ -0,0 +1,60 @@
+import {
+  IExecDataProtector,
+  IExecDataProtectorCore,
+  IExecDataProtectorSharing,
+} from '@iexec/dataprotector';
+import { type Connector } from 'wagmi';
+import { useUserStore } from '../stores/user.store.ts';
+
+let dataProtector: IExecDataProtectorCore | null = null;
+let dataProtectorSharing: IExecDataProtectorSharing | null = null;
+
+export function cleanDataProtectorSDK() {
+  dataProtector = null;
+}
+
+export async function initDataProtectorSDK({
+  connector,
+}: {
+  connector?: Connector;
+}) {
+  const provider = await connector?.getProvider();
+
+  const iexecOptions = {
+    smsURL: import.meta.env.VITE_SMS_URL || undefined,
+    iexecGatewayURL: import.meta.env.VITE_IEXEC_GATEWAY_URL || undefined,
+    // Where user-specific encrypted data are uploaded (consumeProtectedData())
+    resultProxyURL: import.meta.env.VITE_RESULT_PROXY_URL || undefined,
+  };
+
+  const dataProtectorOptions = {
+    dataprotectorContractAddress: import.meta.env.VITE_DATAPROTECTOR_ADDRESS,
+    sharingContractAddress: import.meta.env.VITE_DATAPROTECTOR_SHARING_ADDRESS,
+    subgraphUrl: import.meta.env.VITE_DATAPROTECTOR_SUBGRAPH_URL,
+    ipfsGateway: import.meta.env.VITE_IPFS_GATEWAY_URL,
+    ipfsNode: import.meta.env.VITE_IPFS_NODE_URL,
+    iexecOptions,
+  };
+
+  const dataProtectorParent = new IExecDataProtector(
+    provider,
+    dataProtectorOptions
+  );
+
+  dataProtector = dataProtectorParent.core;
+  dataProtectorSharing = dataProtectorParent.sharing;
+}
+
+export async function getDataProtectorClient(): Promise<{
+  dataProtector: IExecDataProtectorCore;
+  dataProtectorSharing: IExecDataProtectorSharing;
+}> {
+  if (!dataProtector) {
+    const connector = useUserStore.getState().connector;
+    await initDataProtectorSDK({ connector });
+  }
+  if (!dataProtector || !dataProtectorSharing) {
+    throw new Error('iExecDataProtector is not initialized');
+  }
+  return { dataProtector, dataProtectorSharing };
+}
