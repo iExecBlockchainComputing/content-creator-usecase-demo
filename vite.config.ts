af0f2902d07267f52bc5e832e81fc8627d4f5c26
ErwanDecoster <wanerd2@gmail.com>
2025-03-04 11:57:23 +0100

    testing subpath in vercel preview

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   // In prod, app will be served under `demo.iex.ec/content-creator` path
   const basePath =
-    env.SET_SUBPATH_FOR_PROD === 'true' ? '/content-creator' : '/';
+    env.SET_SUBPATH_FOR_PROD !== 'true' ? '/content-creator' : '/';
   console.log('[vite] Building with base path:', basePath);
   return {
     base: basePath,
